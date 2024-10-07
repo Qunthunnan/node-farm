1e43f326faa3c4769bfa9d363853aff8a090f443
Qunthunnan <bredtv6@gmail.com>
2024-10-07 21:59:41 +0300

    base endpoins and data sending

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { error } from "console";
 import fs from "fs";
+import path from "path";
 import http from "http";
+import ulr from "url";
 
 //Blocking synchronius code
 // const test: string = fs.readFileSync("./txt/read-this.txt", "utf-8");
@@ -34,9 +36,48 @@ import http from "http";
 // });
 
 //http server
+
+const data = fs.readFileSync(`./dev-data/data.json`);
+
 const server = http.createServer((request, result) => {
+	const urlPath = request.url;
+
+	switch (urlPath) {
+		case "/overview": {
+			result.end('"Overwiew content"');
+			break;
+		}
+		case "/admin": {
+			result.end('"Admin panel');
+			break;
+		}
+		case "/": {
+			result.end('"Main page"');
+			break;
+		}
+		case "/api": {
+			if (data) {
+				result.writeHead(200, "OK", {
+					"Content-Type": "application/json",
+				});
+				result.end(data);
+			} else {
+				result.writeHead(500, "Server error: data not found", {
+					"Content-type": "text/html",
+				});
+				result.end("<h1>Data error</h1>");
+			}
+		}
+		default: {
+			result.writeHead(404, "Not found 404", {
+				"Content-type": "text/html",
+				"my-own-headers-key": "my-own-headers-data",
+			});
+			result.end(`<h1>Not found</h1>`);
+		}
+	}
+
 	console.log(`GET`);
-	result.end("Helo world!!!🤩");
 });
 
 server.listen(5000, "localhost", () => {
